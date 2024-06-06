90f3648d48f0d5a76b2d21ab9bbf4482f51d11ff
raphx <paulus060202@gmail.com>
2024-06-06 21:37:31 +0700

    init + answer for homework week 9

--- app/src/main/java/com/paulus/todoapp/util/Util.kt
+++ app/src/main/java/com/paulus/todoapp/util/Util.kt
@@ -8,7 +8,7 @@ import com.paulus.todoapp.model.TodoDatabase
 val MIGRATION_1_2 = object : Migration(2, 3) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL(
-            "ALTER TABLE todo ADD COLUMN is_done INTEGER DEFAULT 0 not null")
+            "ALTER TABLE todo ADD COLUMN is_done INTEGER DEFAULT 0 not null") //because there's no boolean data type function in sqlite
     }
 }
 
